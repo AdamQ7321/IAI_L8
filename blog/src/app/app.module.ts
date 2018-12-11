76185a32c4ed23fa741f74747e7b15ee71a4e11e
AdamQ7321 <30066@student.pwsztar.edu.pl>
2018-12-11 12:17:46 +0100

    komentarz

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -7,6 +7,31 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MapComponent } from './components/map/map.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { BlogComponent } from './components/blog/blog.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const approutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'map',
+    component: MapComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  }];
+
 
 
 @NgModule({
@@ -17,11 +42,16 @@ import { QuizComponent } from './components/quiz/quiz.component';
     ContactComponent,
     MapComponent,
     QuizComponent,
+    BlogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
